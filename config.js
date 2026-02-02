3c9241fdd60c765ca25e5777cd658c550fd92b5e
naryto99900532-ops <naryto99900532@gmail.com>
2026-02-03 01:39:39 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// Конфигурация Supabase
+const SUPABASE_CONFIG = {
+    url: 'https://dkyqegxdcerlqnfvodjd.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRreXFlZ3hkY2VybHFuZnZvZGpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwNTc2NzQsImV4cCI6MjA4NTYzMzY3NH0.n6KO-IQMZboeOMCnJQw6gAs8DdWFDZpevAOWBQrxFWA'
+};
+
+// Роли пользователей
+const USER_ROLES = {
+    OWNER: 'owner',
+    ADMIN: 'admin',
+    PLAYER: 'player'
+};
